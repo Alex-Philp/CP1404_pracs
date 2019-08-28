b1d457dd6586e3dda97f110a273b81ab0ec9a447
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-08-28 20:53:07 +1000

    Add and finish woefully inadequate password checker

--- practical_04/list_excersises.py
+++ practical_04/list_excersises.py
@@ -1,6 +1,6 @@
 numbers = []
 total = 0
-for i in range(1,6):
+for i in range(1, 6):
     number = [int(input("Number: "))]
     numbers = numbers + number
 
@@ -11,4 +11,12 @@ numbers.sort()
 print("The smallest number is {}".format(numbers[0]))
 print("The largest number is {}".format(numbers[-1]))
 
-print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
+# woefully inadequate password checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username_input = input("Please enter your username: ")
+if username_input in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
