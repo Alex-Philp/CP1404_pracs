ac172213986f5052593c97f598b1e295494d7b8c
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-08-29 11:42:08 +1000

    Fix missed instructions

--- practical_04/quick_picks.py
+++ practical_04/quick_picks.py
@@ -1,5 +1,10 @@
 def main():
     number_of_quick_picks = int(input("How many quick picks? "))
+
+    while number_of_quick_picks < 0:
+        print("I'm afraid I can't do that")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
     for i in range(0, number_of_quick_picks):
         quick_pick = generate_quick_pick()
         print(quick_pick)
@@ -14,6 +19,8 @@ def generate_quick_pick():
         rand_number = [numbers[rand_index]]
         numbers.remove(int(rand_number[0]))
         quick_pick = quick_pick + rand_number
+
+    quick_pick.sort()
     return quick_pick
 
 
