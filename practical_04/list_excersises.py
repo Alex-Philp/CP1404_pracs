3987a31cd22050c29cf0cbac4584a85429756fe1
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-08-29 11:16:19 +1000

    Fix various minor formatting convention errors

--- practical_04/list_excersises.py
+++ practical_04/list_excersises.py
@@ -14,7 +14,8 @@ print("The largest number is {}".format(numbers[-1]))
 print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
 
 # woefully inadequate password checker
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 username_input = input("Please enter your username: ")
 if username_input in usernames:
     print("Access Granted")
