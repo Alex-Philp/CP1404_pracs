36894ac9279c01f2372bc625bbd313492a472452
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-09-04 22:45:03 +1000

    Add intermediate exercise hex colours

---
+++ practical_05/hex_colours.py
@@ -0,0 +1,12 @@
+HEX_ADDRESS_TO_NAME = {"blue 1": "#0000ff", "GreenYellow": "#adff2f", "MediumSpringGreen": "#00fa9a",
+                       "orchid2": "#ee7ae9", "PaleTurquoise1": "#bbffff", "PapayaWhip": "#ffefd5", "purple4": "#551a8b",
+                       "red1": "#ff0000","YellowGreen": "#9acd32", "tomato3": "#cd4f39"}
+
+colour = (input("Enter colour name: ")).lower()
+while colour != "":
+    if colour in HEX_ADDRESS_TO_NAME:
+        print(HEX_ADDRESS_TO_NAME[colour])
+    else:
+        print("Invalid colour name")
+    colour = input("Enter colour name: ")
+
