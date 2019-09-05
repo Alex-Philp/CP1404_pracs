867341aa7de5555eeb7646ebd096f385a8dc744d
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-09-05 11:18:25 +1000

    Fix formatting regarding PEP 8

--- practical_05/hex_colours.py
+++ practical_05/hex_colours.py
@@ -1,6 +1,6 @@
 HEX_ADDRESS_TO_NAME = {"blue 1": "#0000ff", "GreenYellow": "#adff2f", "MediumSpringGreen": "#00fa9a",
                        "orchid2": "#ee7ae9", "PaleTurquoise1": "#bbffff", "PapayaWhip": "#ffefd5", "purple4": "#551a8b",
-                       "red1": "#ff0000","YellowGreen": "#9acd32", "tomato3": "#cd4f39"}
+                       "red1": "#ff0000", "YellowGreen": "#9acd32", "tomato3": "#cd4f39"}
 
 colour = (input("Enter colour name: ")).lower()
 while colour != "":
@@ -9,4 +9,3 @@ while colour != "":
     else:
         print("Invalid colour name")
     colour = input("Enter colour name: ")
-
