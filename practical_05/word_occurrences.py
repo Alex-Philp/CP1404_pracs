4221a9c8af9c4bcb8f33ef0ef5cca17563211207
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-09-09 15:02:39 +1000

    Change how the code stores the words to be more efficient

--- practical_05/word_occurrences.py
+++ practical_05/word_occurrences.py
@@ -2,19 +2,16 @@ text = input("Text: ")
 word_to_count = {}
 words = text.split(" ")
 for word in words:
-    if word in word_to_count:
+    if word.lower() in word_to_count:
         word_to_count[word] += 1
     else:
-        word_to_count[word] = 1
-strings = []
+        word_to_count[word.lower()] = 1
 longest_word = words[0]
 for word in words:
     if len(word) > len(longest_word):
         longest_word = word
-
-for word, count in word_to_count.items():
-    strings.append(("{:{}} : {}".format(word, len(longest_word), word_to_count[word])))
-strings.sort()
+words = list(word_to_count.keys())
+words.sort()
 print("Text: {}".format(text))
-for string in strings:
-    print(string)
+for word in words:
+    print(("{:{}} : {}".format(word, len(longest_word), word_to_count[word])))
