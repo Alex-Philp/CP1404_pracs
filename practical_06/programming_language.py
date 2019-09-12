b109e1afe914ee1787c37747b4332760a6a579a6
Alex-Philp <alexander.philp@my.jcu.edu.au>
2019-09-12 11:44:08 +1000

    Add initial exercises from prac_06

---
+++ practical_06/programming_language.py
@@ -0,0 +1,16 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name="", typing="", reflection="", year=int):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return 1
+        else:
+            return 0
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
---
+++ practical_06/used_cars.py
@@ -0,0 +1,24 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from practical_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car("my_car",180)
+    limo = Car("limo", 100)
+    limo.add_fuel(20)
+    print("The {} has {} fuel".format(limo.name, limo.fuel))
+    limo.drive(115)
+    print("The {} has driven {} km".format(limo.name, limo.odometer))
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+
+main()
