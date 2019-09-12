c9fcc07ecb1b835a7bb3f75d480376820c69b92e
Alex-Philp <alexander.philp@my.jcu.edu.au>
2019-09-12 11:44:49 +1000

    Change constant name to be accurate

--- practical_05/hex_colours.py
+++ practical_05/hex_colours.py
@@ -1,11 +1,11 @@
-HEX_ADDRESS_TO_NAME = {"blue 1": "#0000ff", "GreenYellow": "#adff2f", "MediumSpringGreen": "#00fa9a",
+NAME_TO_HEX_ADDRESS = {"blue 1": "#0000ff", "GreenYellow": "#adff2f", "MediumSpringGreen": "#00fa9a",
                        "orchid2": "#ee7ae9", "PaleTurquoise1": "#bbffff", "PapayaWhip": "#ffefd5", "purple4": "#551a8b",
                        "red1": "#ff0000", "YellowGreen": "#9acd32", "tomato3": "#cd4f39"}
 
 colour = (input("Enter colour name: ")).lower()
 while colour != "":
-    if colour in HEX_ADDRESS_TO_NAME:
-        print(HEX_ADDRESS_TO_NAME[colour])
+    if colour in NAME_TO_HEX_ADDRESS:
+        print(NAME_TO_HEX_ADDRESS[colour])
     else:
         print("Invalid colour name")
     colour = input("Enter colour name: ")
