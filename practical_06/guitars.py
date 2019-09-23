c32ba2be96c44f298dec6ea50b8917390adbd65e
Alex Philp <alexander.philp@my.jcu.edu.au>
2019-09-23 20:21:19 +1000

    Make code store cost as a float and display to 2 decimal places

--- practical_06/guitars.py
+++ practical_06/guitars.py
@@ -2,12 +2,12 @@ from practical_06.guitar import Guitar
 print("My guitars")
 guitars = []
 name = input("Name: ")
-year = input("Year: ")
-cost = input("Cost: ")
+year = int(input("Year: "))
+cost = float(input("Cost: "))
 i = 0
 while name != "":
     guitars.append(Guitar(name, year, cost))
-    print("{} ({}) : ${} added.".format(guitars[i].name, guitars[i].year, guitars[i].cost))
+    print("{} ({}) : ${:.2f} added.".format(guitars[i].name, guitars[i].year, guitars[i].cost))
     i += 1
     name = input("Name: ")
     if name != "":
@@ -19,4 +19,4 @@ for i, guitar in enumerate(guitars, 1):
         vintage_string="(vintage)"
     else:
         vintage_string=""
-    print("Guitar {}: {} ({}), worth $ {} {}".format(i, guitar.name, guitar.year, guitar.cost, vintage_string))
+    print("Guitar {}: {} ({}), worth $ {:.2f} {}".format(i, guitar.name, guitar.year, guitar.cost, vintage_string))
